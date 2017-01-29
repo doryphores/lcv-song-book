4c374e0c3292be2df0a5d502742e06521360e13f
Martin Laine <martin.laine@gmail.com>
2017-01-29 01:42:39 +0000

    Some keyboard navigation for the sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,28 +8,74 @@ import Icon from './icon'
 class Sidebar extends React.Component {
   constructor () {
     super()
-    this.state = { search: '' }
-    this.handleKeyup = this.handleKeyup.bind(this)
+    this.state = {
+      search: '',
+      highlighted: -1,
+      searching: false
+    }
+    this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
   componentDidMount () {
     if (this.props.selectedSong) {
       document.querySelector('.sidebar__menu-item--selected').scrollIntoView()
     }
-    window.addEventListener('keyup', this.handleKeyup)
+    window.addEventListener('keydown', this.handleKeyDown)
   }
 
   componentWillUnmount () {
-    window.removeEventListener('keyup', this.handleKeyup)
+    window.removeEventListener('keydown', this.handleKeyDown)
   }
 
-  handleKeyup (e) {
-    if (['INPUT', 'TEXTAREA'].includes(e.target.tagName)) return
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.highlighted - this.state.highlighted) {
+      let highlightedElement = this.refs[`item-${this.state.highlighted}`]
+      let listRect = this.refs.songList.getBoundingClientRect()
+      let itemRect = highlightedElement.getBoundingClientRect()
+      if (itemRect.bottom > listRect.bottom) {
+        highlightedElement.scrollIntoView(false)
+      } else if (itemRect.top < listRect.top) {
+        highlightedElement.scrollIntoView(true)
+      }
+    }
+  }
+
+  handleKeyDown (e) {
     switch (e.which) {
-      case 83:
+      case 83: // S
+        if (['INPUT', 'TEXTAREA'].includes(e.target.tagName)) return
         this.refs.searchInput.focus()
+        e.preventDefault()
+        break
+      case 40: // Down arrow
+        if (!this.state.searching) return
+        e.preventDefault()
+        this.updateHighlighted(this.state.highlighted + 1)
+        break
+      case 38: // Up arrow
+        if (!this.state.searching) return
+        e.preventDefault()
+        this.updateHighlighted(this.state.highlighted - 1)
+        break
+      case 13: // Enter
+        if (!this.state.searching || this.state.highlighted === -1) return
+        this.props.onSelect(this.filterSongs()[this.state.highlighted].title)
+        break
+      case 27: // Escape
+        if (!this.state.searching) return
+        this.refs.searchInput.blur()
         break
     }
+  }
+
+  updateHighlighted (value) {
+    let listLength = this.filterSongs().length
+    if (value < 0) {
+      value = listLength - 1
+    } else if (value >= listLength) {
+      value = 0
+    }
+    this.setState({ highlighted: value })
   }
 
   filterSongs () {
@@ -38,15 +84,36 @@ class Sidebar extends React.Component {
     return this.props.songs.filter(s => pattern.test(s.title))
   }
 
+  startSearch () {
+    this.setState({
+      searching: true
+    })
+  }
+
+  stopSearch () {
+    this.setState({
+      searching: false,
+      highlighted: -1
+    })
+  }
+
+  handleSearch (e) {
+    this.setState({
+      search: e.target.value,
+      highlighted: -1
+    })
+  }
+
   classNames (classNames) {
     return classnames(classNames, this.props.className, {
       'sidebar--collapsed': !this.props.visible
     })
   }
 
-  itemClassNames (title) {
+  itemClassNames (title, index) {
     return classnames('sidebar__menu-item', {
-      'sidebar__menu-item--selected': title === this.props.selectedSong.title
+      'sidebar__menu-item--selected': title === this.props.selectedSong.title,
+      'sidebar__menu-item--highlighted': index === this.state.highlighted
     })
   }
 
@@ -59,17 +126,21 @@ class Sidebar extends React.Component {
             placeholder='Search'
             className='sidebar__search-field field__input'
             value={this.state.search}
-            onChange={(e) => this.setState({ search: e.target.value })} />
+            onFocus={this.startSearch.bind(this)}
+            onBlur={this.stopSearch.bind(this)}
+            onChange={this.handleSearch.bind(this)} />
           <Icon icon='search' className='sidebar__icon sidebar__search-icon' />
           <Icon icon='close'
             className='sidebar__icon sidebar__close-icon'
             style={{ display: this.state.search === '' ? 'none' : 'block' }}
             onClick={() => this.setState({ search: '' })} />
         </div>
-        <ul className='sidebar__menu u-flex__panel u-flex__panel--grow'>
-          {this.filterSongs().map(s => (
+        <ul ref='songList'
+          className='sidebar__menu u-flex__panel u-flex__panel--grow'>
+          {this.filterSongs().map((s, i) => (
             <li key={s.title}
-              className={this.itemClassNames(s.title)}
+              ref={`item-${i}`}
+              className={this.itemClassNames(s.title, i)}
               onClick={() => this.props.onSelect(s.title)}>
               {s.title}
             </li>
