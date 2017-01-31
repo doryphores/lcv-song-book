31987240116725bcdf104fe9f88800c3eeb28b67
Martin Laine <martin.laine@simplybusiness.co.uk>
2017-01-31 13:56:34 +0000

    Save and restore window position and dimensions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path'
+import { outputJSON, readJSON } from 'fs-extra'
 
 let win
+
+const CONFIG_PATH = path.join(
+  app.getPath('userData'),
+  'window.json'
+)
 
 app.on('ready', () => {
   // ========================================================
   // Window setup
 
-  win = new BrowserWindow({
+  let opts = {
     minWidth: 800,
     minHeight: 600,
     center: true,
     show: false,
     autoHideMenuBar: true
+  }
+
+  readJSON(CONFIG_PATH, (_, config) => {
+    if (config) Object.assign(opts, config)
+
+    win = new BrowserWindow(opts)
+
+    win.once('ready-to-show', () => win.show())
+
+    win.on('close', () => {
+      let [ width, height ] = win.getSize()
+      let [ x, y ] = win.getPosition()
+      outputJSON(CONFIG_PATH, {
+        width,
+        height,
+        x,
+        y
+      })
+    })
+
+    win.loadURL(`file://${path.join(app.getAppPath(), 'static', 'index.html')}`)
   })
-
-  win.once('ready-to-show', () => win.show())
-
-  win.loadURL(`file://${path.join(app.getAppPath(), 'static', 'index.html')}`)
 })
