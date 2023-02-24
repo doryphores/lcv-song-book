40585de5c7c0b3413253dff6c752e91146676aba
Martin Laine <martin.laine@gmail.com>
2023-02-24 11:35:54 +0000

    Use api to manage store cache

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,36 +1,23 @@
-import { remote } from 'electron'
-import { outputJsonSync, readJSON } from 'fs-extra'
-import * as path from 'path'
-import { createStore, combineReducers, applyMiddleware, Store } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import * as reducers from './reducers'
 import { RESTORE } from '../actions'
 
-const CACHE_PATH = path.join(
-  remote.app.getPath('userData'),
-  'store.json'
-)
+export async function configureStore () {
+  const data = await api.readStore()
+  const rootReducer = combineReducers<ApplicationState>(reducers)
+  const store = createStore(
+    rootReducer,
+    rootReducer(data, { type: RESTORE }),
+    applyMiddleware(thunk)
+  )
 
-export function configureStore (): Promise<Store<ApplicationState>> {
-  return new Promise((resolve, reject) => {
-    readJSON(CACHE_PATH, (_, data) => {
-      try {
-        const rootReducer = combineReducers<ApplicationState>(reducers)
-        const store = createStore(
-          rootReducer,
-          rootReducer(data, { type: RESTORE }),
-          applyMiddleware(thunk)
-        )
+  api.onActionDispatch((actionType) => store.dispatch({ type: actionType }))
 
-        window.addEventListener('beforeunload', () => {
-          outputJsonSync(CACHE_PATH, store.getState())
-        })
-
-        resolve(store)
-      } catch (err) {
-        reject(err)
-      }
-    })
+  window.addEventListener('beforeunload', () => {
+    api.writeStore(store.getState())
   })
+
+  return store
 }
