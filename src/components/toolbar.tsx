cc860a8166336980b29e6c6c67a526395a478e83
Martin Laine <martin.laine@gmail.com>
2023-02-24 11:32:41 +0000

    Convert Toolbar to SFC

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,21 +1,20 @@
 import classnames from 'classnames'
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useCallback } from 'react'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
 
 import { selectVoice } from '../actions'
 import { sheetMusicURL } from '../selectors'
-import Scraper from './scraper'
 import Downloader from './downloader'
 import Settings from './settings'
 import Shortcuts from './shortcuts'
 import Icon from './icon'
 
-interface ToolbarProps {
-  readonly className: string
-  readonly selectedVoice: string
-  readonly sheetMusicURL: string
-  readonly onSelectVoice: (voice: string) => void
+type ToolbarProps = {
+  className: string
+  selectedVoice: string
+  sheetMusicURL: string
+  onSelectVoice: (voice: string) => void
 }
 
 const VOICES = [
@@ -29,40 +28,41 @@ const VOICES = [
   'Bass 2'
 ]
 
-class Toolbar extends React.Component<ToolbarProps> {
-  selectVoice (e: ChangeEvent<HTMLSelectElement>) {
-    this.props.onSelectVoice(e.target.value)
+const Toolbar: React.FC<ToolbarProps> = ({
+  className,
+  selectedVoice,
+  sheetMusicURL,
+  onSelectVoice
+}) => {
+  const selectVoice = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+    onSelectVoice(e.target.value)
     e.target.blur()
-  }
+  }, [selectedVoice])
 
-  render () {
-    return (
-      <div className={classnames(this.props.className, 'toolbar u-flex u-flex--horizontal')}>
-        <div className='toolbar__voice-selector u-flex__panel theme--dark'>
-          <label className='field field--dropdown'>
-            <select className='field__input field__input--select'
-              value={this.props.selectedVoice}
-              onChange={this.selectVoice.bind(this)}>
-              {VOICES.map(v => <option key={v}>{v}</option>)}
-            </select>
-            <Icon className='field__icon' icon='arrow_drop_down' />
-          </label>
-        </div>
-
-        <Scraper className='u-flex__panel' />
-        <Downloader className='u-flex__panel'
-          pdfURL={this.props.sheetMusicURL} />
-        <Settings className='u-flex__panel' />
-        <Shortcuts className='u-flex__panel' />
+  return (
+    <div className={classnames(className, 'toolbar u-flex u-flex--horizontal')}>
+      <div className='toolbar__voice-selector u-flex__panel theme--dark'>
+        <label className='field field--dropdown'>
+          <select className='field__input field__input--select'
+            value={selectedVoice}
+            onChange={selectVoice}>
+            {VOICES.map(v => <option key={v}>{v}</option>)}
+          </select>
+          <Icon className='field__icon' icon='arrow_drop_down' />
+        </label>
       </div>
-    )
-  }
+
+      <Downloader className='u-flex__panel' pdfURL={sheetMusicURL} />
+      <Settings className='u-flex__panel' />
+      <Shortcuts className='u-flex__panel' />
+    </div>
+  )
 }
 
 function mapStateToProps (state: ApplicationState) {
   return {
     selectedVoice: state.selectedVoice,
-    sheetMusicURL: sheetMusicURL(state)!
+    sheetMusicURL: sheetMusicURL(state)
   }
 }
 
