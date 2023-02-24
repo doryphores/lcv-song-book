700ef0ef363438cf36db689dbb0f9a2b1d2e7644
Martin Laine <martin.laine@gmail.com>
2023-02-24 11:35:00 +0000

    Use api for file download

--- src/components/downloader.tsx
+++ src/components/downloader.tsx
@@ -1,9 +1,4 @@
-import { remote, shell } from 'electron'
-import fs from 'fs'
-import { last } from 'lodash'
-import path from 'path'
 import React from 'react'
-import request from 'request'
 
 import ToolbarPanel from './toolbar_panel'
 
@@ -12,18 +7,9 @@ interface DownloaderProps {
   readonly pdfURL: string
 }
 
-function download (pdfURL: string) {
-  const filename = decodeURIComponent(last(pdfURL.split('/'))!)
-  const downloadPath = path.join(remote.app.getPath('downloads'), filename)
-  const stream = fs.createWriteStream(downloadPath)
-  request(pdfURL).pipe(stream).on('finish', () => {
-    shell.openItem(downloadPath)
-  })
-}
-
-const Downloader: React.SFC<DownloaderProps> = ({ pdfURL, className }) => (
+const Downloader: React.FC<DownloaderProps> = ({ pdfURL, className }) => (
   <ToolbarPanel className={className}
-    onToggle={() => download(pdfURL)}
+    onToggle={() => api.downloadFile(pdfURL)}
     disabled={pdfURL === ''}
     toggleIcon='cloud_download' />
 )
