a070eb6e0290559904eff2fe5a598e8c52ebb521
Martin Laine <martin.laine@gmail.com>
2023-02-24 11:36:14 +0000

    Fix types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,35 +1,10 @@
 declare module 'electron-devtools-installer'
 
-declare module 'pdfjs-dist' {
-  interface PDFViewport {
-    height: number
-    width: number
-  }
+declare module '*.png';
+declare module '*.svg';
+declare module '*.worker';
 
-  interface PDFRenderPageOptions {
-    canvasContext: CanvasRenderingContext2D
-    viewport: PDFViewport
-  }
-
-  export interface PDFPage {
-    getViewport(ratio: number): PDFViewport
-    render(opts: PDFRenderPageOptions): Promise<void>
-  }
-
-  export interface PDFDocument {
-    numPages: number
-    getPage(num: number): Promise<PDFPage>
-  }
-
-  interface PDFJSStatic {
-    GlobalWorkerOptions: { workerSrc: string }
-    getDocument(url: string): Promise<PDFDocument>
-  }
-
-  const PDFJS: PDFJSStatic
-
-  export default PDFJS
-}
+declare const api: typeof import("./api").default
 
 type ScrapedResource = Readonly<Record<'title' | 'voice' | 'url', string>>
 
@@ -53,7 +28,7 @@ interface PlaylistState {
   readonly playlists: PlaylistCollection
 }
 
-interface SongMarkers extends Array<number> {}
+type SongMarkers = Array<number>
 
 interface MarkerCollection {
   readonly [key: string]: SongMarkers
@@ -83,6 +58,7 @@ interface ApplicationState {
   readonly markers: MarkerCollection
   readonly notifications: SavedNotification[]
   readonly settings: {
+    readonly username: string
     readonly password: string
     readonly lastResourceUpdate: number
   }
